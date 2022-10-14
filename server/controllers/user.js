8e6846feb3613a51389a8cfeb3c228975c6586e7
masoud <masoud.moradi016@gmail.com>
2022-10-14 02:43:17 -0700

    added heper file for vlidate data when register new user and bcrypt pasword user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,6 @@
+const { validateEmail, validateLength } = require("../helpers/validation");
 const User = require("../models/User");
-
+const bcrypt = require("bcrypt");
 exports.register = async (req, res) => {
   try {
     const {
@@ -14,6 +15,43 @@ exports.register = async (req, res) => {
       gender,
     } = req.body;
 
+    if (!validateEmail(email)) {
+      return res.status(400).json({
+        mesage: "invalid email address",
+      });
+    }
+
+    const check = await User.findOne({ email });
+
+    if (check) {
+      return res.status(400).json({
+        mesage:
+          "this email address already exists,try with different email address",
+      });
+    }
+
+    if (!validateLength(first_name, 3, 30)) {
+      return res.status(400).json({
+        mesage: "first name between 3 and 30 charachters",
+      });
+    }
+
+    if (!validateLength(last_name, 3, 30)) {
+      return res.status(400).json({
+        mesage: "last name between 4 and 30 charachters",
+      });
+    }
+
+    if (!validateLength(password, 6, 60)) {
+      return res.status(400).json({
+        mesage: "password must be at lastest 6 charachters",
+      });
+    }
+
+    const bcryptedPassword = await bcrypt.hash(password, 12);
+
+    console.log(bcryptedPassword);
+    return;
     const user = await new User({
       first_name,
       last_name,
